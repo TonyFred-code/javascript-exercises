8d659d014bbbbdbd44bbd62a12c6c2aa7a5e8e31
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-08-20 16:40:01 +0100

    Solve 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let lowerCaseString = string.toLowerCase().replace(/\W/g, "");
+    if (lowerCaseString === lowerCaseString.split("").reverse("").join("")) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
