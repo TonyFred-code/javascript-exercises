b627b9e7962d392e028584abbf4a853e431c8316
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-08-20 17:09:11 +0100

    Solve 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldest = array[0];
 
+    for (let i = 1; i < array.length; i++) {
+        let oldestBirthYear = oldest.yearOfBirth;
+        let oldestDeathYear = oldest.yearOfDeath || 2023;
+        let oldestAge = oldestDeathYear - oldestBirthYear;
+        let person = array[i]
+        let personBirthYear = person.yearOfBirth;
+        let personDeathYear = person.yearOfDeath || 2023;
+        let personAge = personDeathYear - personBirthYear;
+        if (personAge > oldestAge) {
+            oldest = person;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
