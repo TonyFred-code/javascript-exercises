c4453526000cc44c8349af48d1fbaaf2d25ab9c2
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-08-20 07:46:10 +0100

    Solve 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = "";
 
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    if (string.trim() === "" || string.length === 0) {
+        return "";
+    }
+
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
